a30aeee5643b8d661642165eef4dbb4cbef61d91
Genbuos <swiftypython18@gmail.com>
2024-06-16 00:55:20 -0400

    create columns

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
-
 st.set_page_config(layout='wide')
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/e442e74a-1c08-40ea-8a25-ede9bb424a25.jfif", width=300)
+    st.image("images/e442e74a-1c08-40ea-8a25-ede9bb424a25.jfif", width=400)
 
-    with col2:
-        st.title("Jordan Mitchell")
-        content = '''Hello! I am a freelance Python Programmer with a specialty in AWS cloud computing. I graduated 
-        in 2024 from Wilmington University with a Undergraduate Certificate in Web App Development.'''
-        st.info(content)
+with col2:
+    st.title("Jordan Mitchell")
+    content = '''Hello! I am a freelance Python Programmer with a specialty in AWS cloud computing. I graduated 
+in 2024 from Wilmington University with a Undergraduate Certificate in Web App Development.'''
+    st.info(content)
+st.write("Below are some projects that I have worked on. Feel free to contact me!")
