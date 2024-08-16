8d76a88021510250cdd6a67bbe8b4b8cea0b0b21
Genbuos <Swiftypython18@gmail.com>
2024-08-16 10:17:30 -0400

    added columns and more content

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout='wide')
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/e442e74a-1c08-40ea-8a25-ede9bb424a25.jfif", width=400)
+    st.image("images/1718722988499.jfif", width=400)
 
 with col2:
     st.title("Jordan Mitchell")
@@ -12,3 +13,21 @@ with col2:
 in 2024 from Wilmington University with a Undergraduate Certificate in Web App Development.'''
     st.info(content)
 st.write("Below are some projects that I have worked on. Feel free to contact me!")
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+df = pandas.read_csv("data.csv",sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image("images/" + row['image'])
+        st.write(f"[Source Code]({row['url']})")
+
