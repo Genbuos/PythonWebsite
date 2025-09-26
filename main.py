b77cd0b5029eb99d978442c57951b19b20e54fa4
Genbuos <Swiftypython18@gmail.com>
2025-09-26 15:00:55 -0400

    models fix

--- main.py
+++ main.py
@@ -83,22 +83,22 @@ for row_idx in range(num_rows):
             # Render .glb model for the first project, otherwise show image
             if proj_idx == 0:
                 st.markdown(
-                    '<iframe src="http://localhost:8000/test.html" width="100%" height="350" frameborder="0" scrolling="no" sandbox="allow-scripts allow-same-origin" loading="lazy"></iframe>',
+                    '<iframe src="https://3dmodels-p.vercel.app/test.html" width="100%" height="350" frameborder="0" scrolling="no" sandbox="allow-scripts allow-same-origin" loading="lazy"></iframe>',
                     unsafe_allow_html=True
                 )
             elif row['title'].lower().startswith('portfolio'):
                 st.markdown(
-                    '<iframe src="https://3dmodels-kjr63imp4-jordan-mitchells-projects-346ebe6c.vercel.app/portfolio_preview.html" width="100%" height="350" frameborder="0" scrolling="no" sandbox="allow-scripts allow-same-origin" loading="lazy"></iframe>',
+                    '<iframe src="https://3dmodels-p.vercel.app/portfolio_preview.html" width="100%" height="350" frameborder="0" scrolling="no" sandbox="allow-scripts allow-same-origin" loading="lazy"></iframe>',
                     unsafe_allow_html=True
                 )
             elif row['title'].lower().startswith('food'):
                 st.markdown(
-                    '<iframe src="https://3dmodels-kjr63imp4-jordan-mitchells-projects-346ebe6c.vercel.app/foodtrax.html" width="100%" height="350" frameborder="0" scrolling="no" sandbox="allow-scripts allow-same-origin" loading="lazy"></iframe>',
+                    '<iframe src="https://3dmodels-p.vercel.app/foodtrax.html" width="100%" height="350" frameborder="0" scrolling="no" sandbox="allow-scripts allow-same-origin" loading="lazy"></iframe>',
                     unsafe_allow_html=True
                 )
             elif row['title'].lower().startswith('bank'):
                 st.markdown(
-                    '<iframe src="https://3dmodels-kjr63imp4-jordan-mitchells-projects-346ebe6c.vercel.app/coin.html" width="100%" height="350" frameborder="0" scrolling="no" sandbox="allow-scripts allow-same-origin" loading="lazy"></iframe>',
+                    '<iframe src="https://3dmodels-p.vercel.app/coin.html" width="100%" height="350" frameborder="0" scrolling="no" sandbox="allow-scripts allow-same-origin" loading="lazy"></iframe>',
                     unsafe_allow_html=True
                 )
             else:
